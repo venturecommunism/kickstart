b5ab86dac9b83f15bc6ace2dee65fba8c0fce2c3
fsociety <anon@anon.com>
2017-05-05 20:23:21 -0400

    install xmonad

--- kickstart.sh
+++ kickstart.sh
@@ -3,3 +3,13 @@ sudo apt install -y git
 
 # general purpose
 sudo apt install -y vim
+
+cd ~
+if [ ! -d "xmonad-ubuntu-conf" ]; then
+  git clone https://github.com/venturecommunism/xmonad-ubuntu-conf
+fi
+if [ ! -d ".xmonad" ]; then
+  mkdir .xmonad
+fi
+cp -r xmonad-ubuntu-conf/* .xmonad
+~/.xmonad/install-xmonad
