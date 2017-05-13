6d3ec24a5d6d229ec48700e67fe34a0c943096f2
fsociety <anon@anon.com>
2017-05-12 22:13:13 -0400

    update and upgrade

--- kickstart.sh
+++ kickstart.sh
@@ -1,8 +1,20 @@
+# prelim
+sudo apt update && apt upgrade
+
 # core
 sudo apt install -y git
 
 # general purpose
-sudo apt install -y vim
+sudo apt install -y vim pavucontrol
+
+# node version manager for nodejs and npm
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+
+# you get the following message
+# => Close and reopen your terminal to start using nvm or run the following to use it now:
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 cd ~
 if [ ! -d "xmonad-ubuntu-conf" ]; then
