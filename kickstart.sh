e1e948ecd6ecf350c884f9001aa36bf05e9918a6
fsociety <anon@anon.com>
2017-05-20 12:01:12 -0400

    bigchain

--- kickstart.sh
+++ kickstart.sh
@@ -31,6 +31,43 @@ sudo gpasswd -a ${USER} docker
 sudo service docker restart
 newgrp docker
 
+#### BIGCHAIN
+# mongodb server
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+
+# sudo service mongod start
+# mkdir /data
+# mkdir /data/db
+sudo mongod --replSet=bigchain-rs
+
+# Python BigChain
+
+sudo apt-get update
+sudo apt-get install -y g++ python3-dev libffi-dev
+
+sudo apt-get install -y python3-pip
+sudo pip3 install --upgrade pip setuptools
+
+sudo pip3 install bigchaindb
+
+bigchaindb -y configure mongodb
+
+bigchaindb start
+
+sudo pip install bigchaindb_driver
+
+# TODO: find out if
+# pip install --upgrade bigchaindb
+# or
+# pip install --upgrade bigchaindb_driver
+# are necessary
+
+#### END BIGCHAIN
+
 # core
 sudo apt install -y git
 
